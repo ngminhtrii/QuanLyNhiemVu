7036134b61c0e26446222daa0cdaaf805066025c
MinhTri <ngminhtri2203@gmail.com>
2024-11-25 04:20:24 +0700

    update be

--- server/src/index.js
+++ server/src/index.js
@@ -12,13 +12,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const URI_DB = process.env.MONGO_DB;
 
-const corsOptions = {
-  origin: ["http://localhost:3000", "https://quanlynhiemvu.onrender.com"], // Cho phép nhiều nguồn
-  credentials: true, // Cho phép gửi cookie nếu cần thiết
-};
-
-app.use(cors(corsOptions));
-
 app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: false }));
