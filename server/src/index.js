b7f9c0eab15afae96dd725f3414e4f150d824d16
MinhTri <ngminhtri2203@gmail.com>
2024-11-25 04:35:21 +0700

    update be and fe

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const URI_DB = process.env.MONGO_DB;
 
+const corsOptions = {
+  origin: "https://quanlynhiemvufe.onrender.com",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: false }));
