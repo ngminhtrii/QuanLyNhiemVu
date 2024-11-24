0fb6f07db19cd37565611605fa8b59fda354db77
MinhTri <ngminhtri2203@gmail.com>
2024-11-25 03:54:02 +0700

    update fe

--- client/src/App.js
+++ client/src/App.js
@@ -10,37 +10,34 @@ import BoardHome from "./pages/BoardHome";
 import TaskDetail from "./pages/TaskDetail";
 
 function App() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const accessToken = localStorage.getItem("logger");
-        dispatch(refreshToken({ accessToken }));
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const accessToken = localStorage.getItem("logger");
+    dispatch(refreshToken({ accessToken }));
+  }, [dispatch]);
 
-    return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route
-                path="/"
-                element={
-                    <RequireAuth>
-                        <DefaultLayout />
-                    </RequireAuth>
-                }
-            >
-                <Route index element={<BoardHome />} />
-                <Route
-                    path="/board/:idBoard/task/:idTask"
-                    element={<TaskDetail />}
-                />
-                <Route path="/board/:id" element={<Board />} />
-                <Route path="/board" element={<BoardHome />} />
-                <Route path="/activate" element={<Activate />} />
-            </Route>
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <DefaultLayout />
+          </RequireAuth>
+        }
+      >
+        <Route index element={<BoardHome />} />
+        <Route path="/board/:idBoard/task/:idTask" element={<TaskDetail />} />
+        <Route path="/board/:id" element={<Board />} />
+        <Route path="/board" element={<BoardHome />} />
+        <Route path="/activate" element={<Activate />} />
+      </Route>
 
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    );
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
--- client/src/redux/thunk/user.js
+++ client/src/redux/thunk/user.js
@@ -2,16 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { postApi } from "../../utils/api";
 
 const updateProfile = createAsyncThunk(
-    "users/updateProfile",
-    async ({ data, token, id }, thunkApi) => {
-        try {
-            const res = await postApi(`/profile/${id}`, data, token);
-            return res.data;
-        } catch (error) {
-            const errMsg = error.response.data.err || error.message;
-            return thunkApi.rejectWithValue(errMsg);
-        }
+  "users/updateProfile",
+  async ({ data, token, id }, thunkApi) => {
+    try {
+      const res = await postApi(`/profile/${id}`, data, token);
+      return res.data;
+    } catch (error) {
+      const errMsg = error.response.data.err || error.message;
+      return thunkApi.rejectWithValue(errMsg);
     }
+  }
 );
 
 export default updateProfile;
